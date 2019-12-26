aa7d48b05998ecb4fd5d419d82d59b323e44f21b
Santiago Peña <santiago.pena@trycore.com.co>
2019-12-26 18:08:55 -0500

    path params implement

--- src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploParamsController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +11,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/params")
 public class EjemploParamsController {
-	
+
+	@GetMapping("/")
+	public String index() {
+		return "params/Index";
+	}
+
 	@GetMapping("/string")
-	public String param(@RequestParam(required = false , defaultValue = "No presente la entrada") String texto, Model model){
-		model.addAttribute("Resultado", "El parametro enviado es "+ texto);
+	public String param(@RequestParam(required = false, defaultValue = "No presente la entrada") String texto,
+			Model model) {
+		model.addAttribute("Resultado", "El parametro enviado es " + texto);
 		return "params/ver";
 	}
+
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String saludo, @RequestParam Integer numero, Model model) {
+		model.addAttribute("Resultado", "El saludo enviado es '" + saludo + "' y el número es '" + numero + "'");
+		return "params/ver";
+	}
+
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest request, Model model) {
+		String saludo = request.getParameter("saludo");
+		Integer numero = null;
+		try {
+			numero = Integer.parseInt(request.getParameter("numero"));
+		} catch (NumberFormatException e) {
+			numero = 0;
+		}
+		model.addAttribute("Resultado", "El saludo enviado es '" + saludo + "' y el número es '" + numero + "'");
+		return "params/ver";
+	}
+
 }
---
+++ src/main/java/com/bolsadeideas/springboot/web/app/controllers/EjemploVariablesRutaController.java
@@ -0,0 +1,32 @@
+package com.bolsadeideas.springboot.web.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/variables")
+public class EjemploVariablesRutaController {
+	
+	@GetMapping("/")
+	public String Index(Model model) {
+		model.addAttribute("titulo", "Enviar parámetros de la ruta (@pathVariable)");
+		return "variables/index";
+	}
+	
+	@GetMapping("/string/{texto}")
+	public String variables(@PathVariable String texto, Model model) {
+		model.addAttribute("titulo", "Recibir parámetros de la ruta (@pathVariable)");
+		model.addAttribute("resultado" , "El texto enviado es "+ texto);
+		return "variables/ver";
+	}
+	
+	@GetMapping("/string/{texto}/{numero}")
+	public String variables(@PathVariable String texto, @PathVariable Integer numero, Model model) {
+		model.addAttribute("titulo", "Recibir parámetros de la ruta (@pathVariable)");
+		model.addAttribute("resultado" , "El texto enviado es "+ texto + "y el número enviado en el path es:" + numero);
+		return "variables/ver";
+	}
+}
